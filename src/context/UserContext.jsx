7b8f1a4098212a37912405b3b888e5ac01f1cbfc
Michael0520 <po2319987@gmail.com>
2022-10-27 03:06:20 +0800

    BUG: 紀錄遇到的 BUG 以及想要實現的功能

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,10 +1,30 @@
 import { createContext, useState } from "react";
 
+/**
+ * FIXME:遇到關於 Hooks 使用錯誤的問題，打開註解即可重現錯誤狀況，
+ * 我不清楚如何使用 Hook 在 function componets 外層
+ *
+ * useLocalstorageState github repo: https://react-hooks.org/docs/useLocalstorageState
+ * 希望能夠 rooks 套件內裡的 useLocalstorageState methods，快速取得 localStorage 內的數值
+ * 並且塞回到 initState 去 覆蓋原有的 state
+ */
+// 這三行是使用方法， import localStorage 近來
+// import { useAuthTokenState, useUserProfileState } from "./localStorage";
+
+// [get,set,remove] = useAuthTokenState()
+// const [authToken, setAuthToken, removeAuthToken] = useAuthTokenState();
+// const [userProfile, setUserProfile, removeUserProfile] = useUserProfileState();
+
 const UserContext = createContext();
 
 export const initState = {
   title: "example",
   iconPath: "https://picsum.photos/200/300",
+
+  // 希望能改成下方的判斷，當 localStorage 有數值時，就會賦予進來，沒有的話就傳預設值
+  // title: userProfile ? userProfile.title : 'example',
+  // iconPath: userProfile ? userProfile.iconPath: ''
+  // token: authToken ? authToken : null,
 };
 
 const UserContextProvider = ({ children }) => {
